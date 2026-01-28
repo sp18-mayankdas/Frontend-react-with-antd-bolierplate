7a4fa1f4f3567d04257fae468683f68ce9546e6b
Mayank Kumar Das <mayankkumardas@Mayanks-MacBook-Pro.local>
2026-01-28 19:03:49 +0530

    Fixed icon type and placement based on image position.

--- src/components/layout/auth-layout/index.tsx
+++ src/components/layout/auth-layout/index.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 import AuthBg from '@assets/Auth-Background.png';
 import { Button } from '@/components/ui';
-import { LeftOutlined } from '@ant-design/icons';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 
 interface IAuthLayoutProps {
@@ -17,9 +17,13 @@ export const AuthLayout = ({ children, imagePosition = 'right' }: IAuthLayoutPro
       <div className={`${imagePosition === 'left' ? 'order-2' : 'order-1'} flex flex-col  p-8`}>
         {/* Top section - Button */}
         {loginPath !== 'login' && (
-          <Link to="/login">
-            <Button type="link" icon={<LeftOutlined />}>
-              Back to website
+          <Link to="/login" className={`${imagePosition === 'left' && 'text-right'}`}>
+            <Button
+              type="link"
+              icon={imagePosition === 'left' ? <RightOutlined /> : <LeftOutlined />}
+              iconPlacement={imagePosition === 'left' ? 'end' : 'start'}
+            >
+              Back to Login
             </Button>
           </Link>
         )}
